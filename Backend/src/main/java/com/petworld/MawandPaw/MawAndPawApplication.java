4abc2cd0ef5ae8016b59ab015c1955a6ad1572cd
mohamedsayedahmd <mohamedsayedahmd@gmail.com>
2023-09-05 13:15:24 +0300

    upload spring boot code

---
+++ Backend/src/main/java/com/petworld/MawandPaw/MawAndPawApplication.java
@@ -0,0 +1,13 @@
+package com.petworld.MawandPaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MawAndPawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MawAndPawApplication.class, args);
+	}
+
+}
